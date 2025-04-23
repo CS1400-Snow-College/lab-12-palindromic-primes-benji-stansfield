e65018c34b2e53a5106c1d6f0ebb7f1e9b6e0692
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-04-22 18:07:52 -0600

    Intro, user input, and exceptions added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿/*Benji Stansfield, 04-20-25, Lab 12 "Palindromic Primes"*/
+Console.Clear();
+
+int nthNumber = 0;
+
+/*Main Screen*/
+Console.WriteLine("This program will find palindromic primes- prime numbers that are the same forward as they are backward.\n");
+do
+{
+    try
+    {
+        Console.Write("Please choose which palindromic prime you would like me to find: ");
+        nthNumber = Convert.ToInt32(Console.ReadLine());
+        if (nthNumber < 1) nthNumber = 0; //ensures the number is 1 or above
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please input a number"); //ensures the user types a number
+    }
+}
+while (nthNumber < 1);
+
